e508e73cbc7a6614b5bbc55abb6a257ce531b289
penguin104 <jd1908_0808@yahoo.co.jp>
2025-01-30 22:36:39 +0900

    ボタン配置

--- lib/home.dart
+++ lib/home.dart
@@ -18,6 +18,91 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
         foregroundColor: Colors.white,
       ),
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      //TODO:前進の命令
+                    },
+                    child: Icon(
+                      Icons.north,
+                    )),
+              ),
+              Expanded(
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      //TODO:左回転の命令
+                    },
+                    child: Icon(
+                      Icons.west,
+                    )),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      //TODO:アクションの命令
+                    },
+                    child: Icon(
+                      Icons.ac_unit_outlined,
+                    )),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      //TODO:右回転の命令
+                    },
+                    child: Icon(
+                      Icons.east,
+                    )),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+              Expanded(
+                child: ElevatedButton(
+                    onPressed: () {
+                      //TODO:後退の命令
+                    },
+                    child: Icon(
+                      Icons.south,
+                    )),
+              ),
+              Expanded(
+                child: SizedBox(
+                  width: 30,
+                  height: 30,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.blue,
       ),
